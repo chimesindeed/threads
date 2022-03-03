46d7d239e46a7da0099f09f3a21b208f3118d4e7
chimesindeed <gratitudeadore@fastmail.com>
2022-03-03 15:37:38 -0500

    collection preview renders collection items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-
 import './collection-preview.styles.scss'
+import CollectionItem from '../collection-item/collection-item.component'
 
 const CollectionPreview = ({title, items}) => {
+
   return (<div className='collection-preview'>
+
     <h1 className='title'>{title.toUpperCase()}</h1>
+
     <div className='preview'>
       {
         items.filter((item, idx) => {
           return idx < 4
-        }).map((item) => {
-          return <div key={item.id}>{item.name}</div>
+        }).map(({ id, ...otherItemProps }) => {
+          return <CollectionItem key={id} {...otherItemProps}/>
         })
       }
     </div>
