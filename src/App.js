09cb4193950c0bd707ecc954a44cfffbafd8b7f7
chimesindeed <gratitudeadore@fastmail.com>
2022-03-02 23:33:43 -0500

    added collection-preview page

--- src/App.js
+++ src/App.js
@@ -1,63 +1,16 @@
 import React from 'react'
+import {Switch, Route} from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
-import { Switch, Route } from 'react-router-dom'
-
-const HatsPage = () => {
-
-  return (
-    <div>
-      <h1> Hats Page </h1>
-    </div>
-  )
-}
-const JacketsPage = () => {
-
-  return (
-    <div>
-      <h1> Jackets Page </h1>
-    </div>
-  )
-}
-const SneakersPage = () => {
-
-  return (
-    <div>
-      <h1> SneakersPage </h1>
-    </div>
-  )
-}
-
-const WomensPage = () => {
-
-  return (
-    <div>
-      <h1> Womens Page </h1>
-    </div>
-  )
-}
-const MensPage = () => {
-
-  return (
-    <div>
-      <h1> Mens Page </h1>
-    </div>
-  )
-}
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
 
-  return (
-    <div>
-      <Switch>
-        <Route exact path= '/' component={ HomePage } />
-        <Route exact path= '/shop/hats' component={ HatsPage } />
-        <Route exact path= '/shop/jackets' component={ JacketsPage } />
-        <Route exact path= '/shop/sneakers' component={ SneakersPage} />
-        <Route exact path= '/shop/womens' component={ WomensPage } />
-        <Route exact path= '/shop/mens' component={ MensPage } />
-      </Switch>
-    </div>
-  )
+  return (<div>
+    <Switch>
+      <Route exact="exact" path='/' component={HomePage}/>
+      <Route exact="exact" path='/shop' component={ShopPage}/>
+    </Switch>
+  </div>)
 }
 export default App
