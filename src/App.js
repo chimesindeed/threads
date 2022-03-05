d7ba4d1f1f85c55f930c8f94450372b74bc30951
chimesindeed <gratitudeadore@fastmail.com>
2022-03-04 20:50:41 -0500

    implemented user authentication with email

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor(){
-    super()
+    super();
+
     this.state = {
       currentUser: null
     }
@@ -23,23 +24,26 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-
           this.setState({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, ()=> { console.log(this.state)});
+          });
         });
-      } else {
-      this.setState({ currentUser: userAuth });
-    }
+
+      }
+
+     this.setState({ currentUser: userAuth });
+
+     console.log(this.state)
     });
   }
 
   componentWillUnmount(){
     this.unsubscribeFromAuth();
   }
+
   render(){
     return (<div>
       <Header currentUser={this.state.currentUser}/>
