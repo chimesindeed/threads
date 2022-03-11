655a42ea6a33966626f4788bb0540ddb8a291965
chimesindeed <gratitudeadore@fastmail.com>
2022-03-10 19:42:19 -0500

    onclick of goto checkout button, cart-dropdown toggles to hidden

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,10 +4,11 @@ import { createStructuredSelector } from 'reselect'
 import { withRouter } from 'react-router-dom'
 import CustomButton from '../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component'
-import { selectCartItems} from '../../redux/cart/cart.selectors'
+import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js'
 import './cart-dropdown.styles.scss'
 
-const CartDropDown = ({cartItems, history}) => {
+const CartDropDown = ({cartItems, history, dispatch }) => {
 
   return (
     <div className='cart-dropdown'>
@@ -22,7 +23,10 @@ const CartDropDown = ({cartItems, history}) => {
             <span className= 'empty-message'>Your cart is empty</span>
         }
       </div>
-      <CustomButton onClick={()=>{history.push('/checkout')}}>GO TO CHECKOUT</CustomButton>
+      <CustomButton onClick={()=> {
+          history.push('/checkout');
+          dispatch(toggleCartHidden());
+      }}>GO TO CHECKOUT</CustomButton>
     </div>
   )
 }
