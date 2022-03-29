c433dceaf97264b9d73b397775f5e8f26665bf4c
chimesindeed <gratitudeadore@fastmail.com>
2022-03-29 04:27:39 -0400

    added redux-persist, to persist cart values across sessions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,25 @@
 
-import { createStore, applyMiddleware } from 'redux'
+import { compose, createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
 
 import rootReducer from './root-reducer'
 
-const middlewares = [logger]
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
+
+const composedEnhancers = compose(applyMiddleware(...middlewares))
+
+const store = createStore(persistedReducer, undefined, composedEnhancers)
+
+export const persistor = persistStore(store)
 
 export default store
